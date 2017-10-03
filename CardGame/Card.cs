39cc3ea84c4dcdc35481fab34e7410f1adeae19f
Liam Sharpe <liamsmail1234@gmail.com>
2017-10-04 08:58:12 +1300

    Initial commit

---
+++ CardGame/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Card
+    {
+        int id;    // 0 ... 51
+        int value; // 1 ... 13
+        char suit; // s, h, c, d
+
+        public Card(int i, int v, char c) {
+            id = i;
+            value = v;
+            suit = c;
+        }
+
+        public char getSuit()
+        {
+            return suit;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
+}
---
+++ CardGame/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Deck
+    {
+        Card[] deck;
+
+        public Deck()
+        {
+
+        }
+
+        public void BuildDeck()
+        {
+            
+            for (int i = 0; i < 52; i++)
+            {
+                Card test = new Card(i, i, 'a');
+            }
+        }
+
+    }
+}
