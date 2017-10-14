d6ee2f874f11b5b539c9465a2e29f4c079ac343c
Liam Sharpe <liamsmail1234@gmail.com>
2017-10-14 18:42:35 +1300

    Working game!

--- CardGame/MainPage.xaml.cs
+++ CardGame/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,6 +25,9 @@ namespace CardGame
     public sealed partial class MainPage : Page
     {
         Deck myDeck = new Deck();
+        int ValueOfLastCard;
+        int NextCardTop;
+        int NextCardBottom;
 
         public MainPage()
         {
@@ -33,11 +38,15 @@ namespace CardGame
 
         private void PlayArea_Loaded(object sender, RoutedEventArgs e)
         {
-            this.PlayArea.Text = "";
-            for (int i = 0; i < myDeck.deckSize; i++)
-            {
-                this.PlayArea.Text += myDeck.deck[i].GetValue().ToString() + " of " + myDeck.deck[i].GetSuit().ToString() + "\n";
-            }
+            this.PlayArea.Text = "Welcome to the game of Up n' Down\n" +
+                "The Aim of this game is to outlast your opponent\n" +
+                "Rules:\n" +
+                "To start the game, simply click the 'Deal' Button\n" +
+                "Then you and your opponent take turns at clicking the 'Player 1 Turn' and 'Player 2 Turn' buttons\n" +
+                "The program will automatically choose a card that is either 1 above or 1 below the card that was just played\n" +
+                "The last person who can still play cards is the winner\n" +
+                "Press 'Deal' now to start playing!" +
+                "\n\n\n\n\n\n";
         }
 
         private void DealButton_Click(object sender, RoutedEventArgs e)
@@ -59,13 +68,30 @@ namespace CardGame
             {
                 myDeck.player1Hand[0].SetStatus(true);
                 myDeck.deck[0].SetStatus(true);
-            } else
-            {
-
+                ValueOfLastCard = myDeck.player1Hand[0].GetValue();
+                PlayArea.Text = "Last card played: " + myDeck.player1Hand[0].GetValue().ToString() + " of " + myDeck.player1Hand[0].GetSuit().ToString();
+                Player1Text();
             }
+            else
+            {
+                SetNextCardValues();
 
-            PlayAreaText();
-            Player1Text();
+                for (int i = 0; i < myDeck.deckSize / 2; i++)
+                {
+                    if ((myDeck.player1Hand[i].GetValue() == NextCardTop || myDeck.player1Hand[i].GetValue() == NextCardBottom) &&
+                    myDeck.player1Hand[i].GetStatus() != true)
+                    {
+                        myDeck.player1Hand[i].SetStatus(true);
+                        myDeck.deck[i].SetStatus(true);
+                        ValueOfLastCard = myDeck.player1Hand[i].GetValue();
+                        PlayArea.Text = "Last card played: " + myDeck.player1Hand[i].GetValue().ToString() + " of " + myDeck.player1Hand[i].GetSuit().ToString();
+                        break;
+                    } else if (i == (myDeck.deckSize / 2) - 1)
+                        EndGameAsync(2);                    
+                }
+
+                Player1Text();
+            }
 
             Player1Button.IsEnabled = false;
             Player2Button.IsEnabled = true;
@@ -73,16 +99,26 @@ namespace CardGame
 
         private void Player2Button_Click(object sender, RoutedEventArgs e)
         {
+            SetNextCardValues();
+
+            for (int i = 0; i < myDeck.deckSize / 2; i++)
+            {
+                if ((myDeck.player2Hand[i].GetValue() == NextCardTop || myDeck.player2Hand[i].GetValue() == NextCardBottom) &&
+                myDeck.player2Hand[i].GetStatus() != true)
+                {
+                    myDeck.player2Hand[i].SetStatus(true);
+                    myDeck.deck[i].SetStatus(true);
+                    ValueOfLastCard = myDeck.player2Hand[i].GetValue();
+                    PlayArea.Text = "Last card played: " + myDeck.player2Hand[i].GetValue().ToString() + " of " + myDeck.player2Hand[i].GetSuit().ToString();
+                    break;
+                } else if (i == (myDeck.deckSize / 2) - 1)
+                    EndGameAsync(1);                
+            }
+
+            Player2Text();
+
             Player1Button.IsEnabled = true;
             Player2Button.IsEnabled = false;
-        }
-
-        private void PlayAreaText()
-        {
-            PlayArea.Text = "";
-            for (int i = 0; i < myDeck.deckSize; i++)
-                if (myDeck.deck[i].GetStatus() == true)
-                    this.PlayArea.Text += myDeck.deck[i].GetValue().ToString() + " of " + myDeck.deck[i].GetSuit().ToString() + "\n";
         }
 
         private void Player1Text()
@@ -99,6 +135,24 @@ namespace CardGame
             for (int i = 0; i < myDeck.deckSize / 2; i++)
                 if (myDeck.player2Hand[i].GetStatus() != true)
                     this.Player2HandBox.Text += myDeck.player2Hand[i].GetValue().ToString() + " of " + myDeck.player2Hand[i].GetSuit().ToString() + "\n";
+        }
+
+        private void SetNextCardValues()
+        {           
+            NextCardTop = ValueOfLastCard + 1;
+            NextCardBottom = ValueOfLastCard - 1;
+            if (NextCardTop == 14)
+                NextCardTop = 1;
+            if (NextCardBottom == 0)
+                NextCardBottom = 13;
+        }
+
+        private async void EndGameAsync(int p)
+        {
+            PlayArea.Text = "Game over";
+            var dialog = new MessageDialog("Player " + p + " Wins!\nPress 'Close' to exit the application");
+            await dialog.ShowAsync();
+            Application.Current.Exit();
         }
     }
 }
